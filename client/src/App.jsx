e93ea0d4cb43a12307252fae247d1776056e70dd
David L <159317073+daviscrib@users.noreply.github.com>
2024-06-05 16:12:36 +0200

    Add map component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
 import Nav from "./components/Nav";
+import Map from "./components/Map";
 import "./main.scss";
 
 function App() {
-  return <Nav />;
+  return (
+    <>
+      <Nav />
+      <Map />
+    </>
+  );
 }
 
 export default App;
