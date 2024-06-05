115200fa790a95d8f9c0fa0d18c919d62f638757
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-06-05 11:46:34 +0200

    Set structure home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,19 @@
 function Home() {
   return (
     <>
-      <h1>hey</h1>
-      <p>coucou</p>
+      <h2>
+        Partez à l&apos;aventure pour découvrir les œuvres d&apos;art autour de
+        chez vous !
+      </h2>
+      <p>map</p> {/* import de la map */}
+      <button type="button"> Regles</button>
+      <p>
+        Pour se faire, aidez-vous de la carte afin de chercher des Street Art
+        dans les rues et vous émerveiller de la créativité d&apos;artistes
+        talentueux.
+      </p>
+      <button type="button"> S'inscrire</button>
+      <button type="button"> Connexion</button>
     </>
   );
 }
