76cb3316370aba7779fbbc9a0bbf9d0953a74ac0
David L <159317073+daviscrib@users.noreply.github.com>
2024-06-06 12:18:23 +0200

    fixed file map.scss

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,22 +1,21 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import "../style/map.scss";
 
 const position = [48.8566, 2.3522];
 function Map() {
   return (
     <div className="div-map">
       <MapContainer
+        className="map-container"
         center={position}
         zoom={13}
         style={{
           height: "40vh",
           width: "80vw",
-          borderBlockStyle: "solid",
-          borderBlockColor: "white",
         }}
-        className="map-container"
       >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' />
       </MapContainer>
     </div>
   );
