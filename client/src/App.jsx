a1d1f85c3f515c959ce9ea53c0dccbce4c7d24f5
KellyJeanneBeck <kbe.kellybeck@gmail.com>
2024-06-09 02:09:56 +0200

    burger menu mobile version done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header openMenu={openMenu} />
-      <Nav isMenuOpen={isMenuOpen} />
+      <Nav openMenu={openMenu} isMenuOpen={isMenuOpen} />
     </div>
   );
 }
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,28 +2,43 @@ import PropTypes from "prop-types";
 import { Link, Outlet } from "react-router-dom";
 import Footer from "./Footer";
 
-function Nav({ isMenuOpen }) {
+function Nav({ openMenu, isMenuOpen }) {
   return (
-    <main>
+    <main className={isMenuOpen ? "order-end" : "order-start"}>
       <nav className={isMenuOpen ? "menu-open" : "menu-close"}>
-        <Link to="/">
-          <p>Acceuil</p>
-        </Link>
-        <Link to="/galery">
-          <p>Galery</p>
-        </Link>
-        <Link to="/instruction">
-          <p>Insctructions</p>
-        </Link>
+        <ul>
+          <Link to="/">
+            <button type="button" onClick={() => openMenu()}>
+              Accueil
+            </button>
+          </Link>
+          <Link to="/galery">
+            <button type="button" onClick={() => openMenu()}>
+              Galerie
+            </button>
+          </Link>
+          <Link to="/instruction">
+            <button type="button" onClick={() => openMenu()}>
+              Règles
+            </button>
+          </Link>
+        </ul>
+        <ul className="log-nav">
+          <button type="button">Connexion</button>
+          <button type="button">Inscription</button>
+        </ul>
       </nav>
-      <Outlet />
-      <Footer />
+      <div className={isMenuOpen ? "hide-content" : "show-content"}>
+        <Outlet />
+        <Footer />
+      </div>
     </main>
   );
 }
 
 Nav.propTypes = {
   isMenuOpen: PropTypes.bool.isRequired,
+  openMenu: PropTypes.func.isRequired,
 };
 
 export default Nav;
