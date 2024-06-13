636b673368523cb932ce52da5866eb5d0774a476
Mohamed Jebri <159800102+memedjjebri@users.noreply.github.com>
2024-06-13 15:42:23 +0200

    fixing map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -8,7 +8,7 @@ function Map() {
       <div className="div-map">
         <MapContainer center={[48.8566, 2.3522]} zoom={13}>
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+           
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           <LocationMarker />
