0c3f9f5492c92153befaf2144b9b91f64006b5bd
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-06-19 12:15:40 +0200

    role MVC set, faudra retravailler au details selon ce qu'on doit faire avec cette table

--- server/app/routers/api/roles/router.js
+++ server/app/routers/api/roles/router.js
@@ -3,14 +3,20 @@ const express = require("express");
 const router = express.Router();
 
 /* ************************************************************************* */
-// Import And Use Routers Here
+// Define Your API Routes Here
 /* ************************************************************************* */
 
-const itemsRouter = require("./items/router");
-const rolesRouter = require('./roles/router');
+// Import item-related actions
+const { browse, read, add } = require("../../../controllers/roleActions");
 
-router.use("/items", itemsRouter);
-router.use("/roles", rolesRouter);
+// Route to get a list of items
+router.get("/", browse);
+
+// Route to get a specific item by ID
+router.get("/:id", read);
+
+// Route to add a new item
+router.post("/", add);
 
 /* ************************************************************************* */
 
