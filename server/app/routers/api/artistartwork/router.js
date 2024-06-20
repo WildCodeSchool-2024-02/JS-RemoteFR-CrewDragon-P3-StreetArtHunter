a888f1e0cfb364df7212d586a26d0088cd12f727
FabienLqr <159054866+FabienLqr@users.noreply.github.com>
2024-06-20 16:10:03 +0200

    delete line useless

--- server/app/routers/api/artistartwork/router.js
+++ server/app/routers/api/artistartwork/router.js
@@ -5,19 +5,10 @@ const router = express.Router();
 const {
   browse,
   read,
-  add,
-  edit,
-  destroy,
 } = require("../../../controllers/ArtistArtworkAction");
 
 router.get("/", browse);
 
 router.get("/:id", read);
-
-router.post("/", add);
-
-router.put("/:id", edit);
-
-router.delete("/:id", destroy);
 
 module.exports = router;
