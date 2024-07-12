93a1bddd75303393caaf4d14bde9e3041afeca5a
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-07-12 10:04:15 +0200

    Merci le trone, et je parle pas d'celui du roi

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -13,25 +13,25 @@ const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [user, setUser] = useState({});
+  const [person, setPerson] = useState({});
   useEffect(() => {
-    console.info("coucou de useEffect", user);
-  }, [user]);
+    console.info("coucou de useEffect", person);
+  }, [person]);
 
-  const login = useCallback((person) => {
-    console.info("Ici c'est ce que tu as recup", person);
-    setUser(person);
-    console.info("after", user);
+  const login = useCallback((someone) => {
+    console.info("Ici c'est ce que tu as recup", someone);
+    setPerson(someone);
+    console.info("after", person);
     
     setIsAuthenticated(true);
-    console.info("coucou tu es co", user);
-  }, [user]);
+    console.info("coucou tu es co", person);
+  }, [person]);
 
   const logout = async () => {
     try {
       await axios.post("/api/auth/logout");
       setIsAuthenticated(false);
-      setUser(null);
+      setPerson(null);
     } catch (error) {
       console.error("Logout error:", error);
       throw new Error("Failed to logout.");
@@ -39,8 +39,8 @@ export function AuthProvider({ children }) {
   };
 
   const value = useMemo(
-    () => ({ isAuthenticated, user, login, logout }),
-    [isAuthenticated, login, user]
+    () => ({ isAuthenticated, person, login, logout }),
+    [isAuthenticated, login, person]
   );
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
