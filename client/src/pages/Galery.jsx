d756657fa45e27dc369dcecd243ef1fde5b2295a
KellyJeanneBeck <kbe.kellybeck@gmail.com>
2024-07-22 15:43:31 +0200

    started to map artwork table in galery page

--- client/src/pages/Galery.jsx
+++ client/src/pages/Galery.jsx
@@ -1,6 +1,26 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 import workArt from "../data/tempGaleryData";
 
+const url = import.meta.env.VITE_API_URL;
+
 function Galery() {
+  const [artworks, setArtworks] = useState([]);
+
+  useEffect(() => {
+    const getArtworks = async () => {
+      try {
+        const res = await axios.get(`${url}/api/artworks`);
+
+        setArtworks(res.data);
+      } catch (error) {
+        console.error("Error: ", error);
+      }
+    };
+    getArtworks();
+  }, []);
+
   return (
     <section className="galery-section">
       <h2>Les oeuvres trouvées par la communauté !</h2>
@@ -14,6 +34,12 @@ function Galery() {
               className="picture"
             />
             <p className="city">{art.city}</p>
+          </article>
+        ))}
+        {artworks.map((artwork) => (
+          <article key={artwork.id} className="cardGalery">
+            <h3 className="name">{artwork.title}</h3>
+            <p>{artwork.description}</p>
           </article>
         ))}
       </div>
