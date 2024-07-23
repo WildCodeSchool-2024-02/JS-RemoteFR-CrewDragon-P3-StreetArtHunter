b5cc7052ffb5f1507a09838993723c6e2cf35022
KellyJeanneBeck <kbe.kellybeck@gmail.com>
2024-07-23 15:51:09 +0200

    fix galery

--- client/src/pages/Galery.jsx
+++ client/src/pages/Galery.jsx
@@ -5,8 +5,18 @@ import workArt from "../data/tempGaleryData";
 
 const url = import.meta.env.VITE_API_URL;
 
+function trouve(id, pictures) {
+  for (let i = 0; i < pictures.length; i += 1) {
+    if (pictures[i].artwork_id === id) {
+      return pictures[i].picture;
+    }
+  }
+  return -1;
+}
+
 function Galery() {
   const [artworks, setArtworks] = useState([]);
+  const [pictures, setPictures] = useState([]);
 
   useEffect(() => {
     const getArtworks = async () => {
@@ -19,6 +29,19 @@ function Galery() {
       }
     };
     getArtworks();
+  }, []);
+
+  useEffect(() => {
+    const getPictures = async () => {
+      try {
+        const res = await axios.get(`${url}/api/pictures`);
+
+        setPictures(res.data);
+      } catch (error) {
+        console.error("Error: ", error);
+      }
+    };
+    getPictures();
   }, []);
 
   return (
@@ -39,6 +62,11 @@ function Galery() {
         {artworks.map((artwork) => (
           <article key={artwork.id} className="cardGalery">
             <h3 className="name">{artwork.title}</h3>
+            <img
+              src={`${import.meta.env.VITE_API_URL}/uploads/${trouve(artwork.id, pictures)}`}
+              alt="L'artwork"
+              className="picture"
+            />
             <p>{artwork.description}</p>
           </article>
         ))}
