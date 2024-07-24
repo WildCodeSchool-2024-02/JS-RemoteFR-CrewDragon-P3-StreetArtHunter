5b491f6639ab51a7a5885e2a31c2f323a2664c97
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-07-24 12:43:21 +0200

    change aut

--- server/app/routers/api/review/router.js
+++ server/app/routers/api/review/router.js
@@ -4,6 +4,7 @@ const multer = require("multer");
 const router = express.Router();
 
 const upload = multer({ dest: "uploads/" });
+const { roleControle } = require("../../../services/admin");
 
 const fileUpload = require("../../../services/fileUpload");
 
@@ -20,15 +21,15 @@ const {
 } = require("../../../controllers/reviewActions");
 
 // Route to get a list of reviews
-router.get("/", browse);
+router.get("/", roleControle, browse);
 // Route to get a specific review by ID
-router.get("/:id", read);
+router.get("/:id", roleControle, read);
 
 // Route to add a new review
 router.post("/", upload.single("picture"), fileUpload, add);
 
 // Route to delete a review by id
-router.delete("/:id", destroy);
+router.delete("/:id", roleControle, destroy);
 
 /* ************************************************************************* */
 
